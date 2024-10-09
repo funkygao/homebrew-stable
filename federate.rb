08106e75973689ada4baed79f3ff3bb5e53c6a2d
gaopeng71 <gaopeng8@jd.com>
2024-10-09 11:09:00 +0800

    upgrade to v2.0.5

--- federate.rb
+++ federate.rb
@@ -1,8 +1,8 @@
 class Federate < Formula
   desc "A tool for merging and deploying microservices"
   homepage "https://github.com/funkygao/federate"
-  url "https://github.com/funkygao/federate/archive/refs/tags/v2.0.4.tar.gz"
-  sha256 "8e544b4053f030dca0c3df3bcf2b18f9fe694ddbceaab986101c58fc1d9b0276"
+  url "https://github.com/funkygao/federate/archive/refs/tags/v2.0.5.tar.gz"
+  sha256 "41f0ea385a4e04aa920dfdb13c2e3a24b46c37719de80581a838cfc1c11ec6d8"
 
   depends_on "go" => :build
 
